aa8491db865d17123662fa1fa84183f92e2a04a4
cleberson.chagas <cleberson.chagas@syngenta.com>
2025-02-27 12:01:46 -0300

    Add root route to server and update port assignment syntax

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,14 @@
 // filepath: /e:/IdeaProjects/school-control/backend/src/server.ts
 import app from './app';
 import dotenv from "dotenv";
+
 dotenv.config();
 
-const port = process.env.PORT ?? 5000;
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Running!');
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
