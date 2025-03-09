11efe4a448a4b78b92989e4a6732c7bb21894521
cleberson.chagas <cleberson.chagas@syngenta.com>
2025-03-09 00:21:47 -0300

    Add sorting functionality to BaseService and its subclasses

--- backend/src/services/ParentService.ts
+++ backend/src/services/ParentService.ts
@@ -1,10 +1,11 @@
 // filepath: /e:/IdeaProjects/school-control/backend/src/services/responsavelService.ts
-import { Responsible, IResponsible } from '@hyteck/shared';
+import {IResponsible, Responsible} from '@hyteck/shared';
 import {BaseService} from "./generics/BaseService";
 
 export class ParentService extends BaseService<IResponsible>{
   constructor() {
     super(Responsible);
     this.populateFields= ['students'];
+    this.sortFields= {name: 1};
   }
 }
